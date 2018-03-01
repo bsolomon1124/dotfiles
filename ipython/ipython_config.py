e472273548c4a459598e2ebd5b4f7ffa1a0676c9
Brad Solomon <brad.solomon.1124@gmail.com>
2018-03-01 11:06:59 -0500

    add ipython_config

---
+++ ipython/ipython_config.py
@@ -0,0 +1,18 @@
+"""IPython (v. 6.0+) configuration file."""
+
+# To create: $ ipython profile create
+# http://ipython.readthedocs.io/en/stable/config/index.html
+# http://ipython.readthedocs.io/en/stable/config/options/terminal.html
+
+import IPython
+
+c = get_config()
+c.TerminalInteractiveShell.prompts_class = IPython.terminal.prompts.ClassicPrompts
+c.TerminalIPythonApp.display_banner = False
+c.InteractiveShell.automagic = False
+c.InteractiveShell.history_load_length = 25000
+c.TerminalInteractiveShell.confirm_exit = False
+c.PlainTextFormatter.max_seq_length = 250
+c.InteractiveShell.xmode = 'Plain'
+c.InteractiveShell.banner1 = ''
+
