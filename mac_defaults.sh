71e37b56e73196de0ff65022fd3cadf960bb3239
Brad Solomon <brad.solomon.1124@gmail.com>
2020-02-15 12:17:02 -0500

    Sensible macOS defaults write

---
+++ mac_defaults.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Set default: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Set default: show hidden files"
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+echo "Set default: autohide the dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "Set default: show battery percentage"
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
