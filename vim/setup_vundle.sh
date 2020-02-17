72dada4c58424df6f6bea4ed786d974daac85b81
Brad Solomon <bsolomon@protonmail.com>
2020-02-16 21:45:34 -0500

    Break out vimrc by complexity

---
+++ vim/setup_vundle.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! -x "$(command -v git)" ]]; then
+    echo "Vundle installation required git" >&2
+    exit 1
+fi
+
+DEST="${HOME}/.vim/bundle/Vundle.vim"
+rm -rfv "$DEST"
+git clone --single-branch \
+    https://github.com/VundleVim/Vundle.vim.git \
+    "$DEST"
+vim +PluginInstall +qall
