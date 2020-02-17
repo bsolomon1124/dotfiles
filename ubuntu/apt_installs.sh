4ea5d5b3b8a87b7680af84be9d3743d1de16005d
Brad Solomon <bsolomon@protonmail.com>
2020-02-16 22:59:20 -0500

    Add an Ubuntu variant

---
+++ ubuntu/apt_installs.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Must be root to run this script"
+    exit 1
+fi
+
+set -ex
+
+apt-get update -y
+apt-get autoremove -y
+
+# Keep this alphabetized for sanity
+apt-get install -y --no-install-recommends build-essential
+apt-get install -y --no-install-recommends bzip2
+apt-get install -y --no-install-recommends dirmngr
+apt-get install -y --no-install-recommends git
+apt-get install -y --no-install-recommends g++
+apt-get install -y --no-install-recommends gcc
+apt-get install -y --no-install-recommends gnupg
+apt-get install -y --no-install-recommends pkg-config
+apt-get install -y --no-install-recommends libc6-dev
+apt-get install -y --no-install-recommends libssl-dev
+apt-get install -y --no-install-recommends llvm
+apt-get install -y --no-install-recommends make
+apt-get install -y --no-install-recommends openssh-client
+apt-get install -y --no-install-recommends tree
+apt-get install -y --no-install-recommends unzip
+apt-get install -y --no-install-recommends wget
+apt-get install -y --no-install-recommends xz-utils
+
+# A few Python/pyenv deps kept separate (some may be above though)
+apt-get install -y --no-install-recommends libssl-dev
+apt-get install -y --no-install-recommends zlib1g-dev
+apt-get install -y --no-install-recommends libbz2-dev
+apt-get install -y --no-install-recommends libreadline-dev
+apt-get install -y --no-install-recommends libsqlite3-dev
+apt-get install -y --no-install-recommends libncurses5-dev
+apt-get install -y --no-install-recommends libncursesw5-dev
+apt-get install -y --no-install-recommends xz-utils
+apt-get install -y --no-install-recommends tk-dev
+apt-get install -y --no-install-recommends libffi-dev
+apt-get install -y --no-install-recommends liblzma-dev
+apt-get install -y --no-install-recommends python-openssl
+
+# Clean up the apt cache
+rm -rfv /var/lib/apt/lists/*
+
